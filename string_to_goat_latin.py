19b69eac5e488522f7b8ead2b8431b42a2ca476e
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-05-23 00:02:29 -0400

    Update string_to_goat_latin.py

--- string_to_goat_latin.py
+++ string_to_goat_latin.py
@@ -18,7 +18,7 @@ def string_to_goat_latin(var):
 
         else:
             temp = var[0]
-            var.replace(temp,'')
+            var = var.replace(temp,'')
             var = var + temp + 'ma'
 
         var = var + add_a
