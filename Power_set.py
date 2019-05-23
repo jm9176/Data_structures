916a1eb2f09d59c046b4695096aad0de4e6f38e0
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-05-22 20:04:50 -0400

    Create Power_set.py

---
+++ Power_set.py
@@ -0,0 +1,26 @@
+# Function to return the power set of the given array
+def power_set(arr):
+   
+   # The final list containing
+   # the power-set of the input array
+   # An temp list ot store the new sets
+   # Count variable to check the output response
+   
+   power_list = [[]]
+   list = []
+   count = 1
+
+   for var in arr:
+       for var1 in power_list:
+           list.extend([var1 + [var]])
+           count += 1
+
+       power_list.extend(list)
+       list.clear()
+
+   print(count)
+   return power_list
+
+# Input array
+arr = [1,2,3,4,5]
+print(power_set(arr))
