64b051018cb78adbeb06b8dda3c1c1852beeeef0
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-05-30 00:56:40 -0400

    Create Scheduling_a_task.py

---
+++ Scheduling_a_task.py
@@ -0,0 +1,24 @@
+'''
+Implementing a job scheduler which takes
+in a function, f, and an integer, n, and
+calls f after n milliseconds.
+'''
+
+import threading
+import time
+
+# Function for scheduling the task
+# which in this case is to print hello
+# f is a function and t_ms is time in millisecs
+def scheduler(f, t_ms):
+    time.sleep(t_ms)
+    return f(int(t_ms*1000))
+
+def print_hello(num):
+    for i in range(num):
+        print threading.currentThread().getName()
+
+# Initializing a thread for the job scheduler
+t1 = threading.Thread(target = scheduler, args=(print_hello, 0.005))
+t1.start()
+time.sleep(1)
