2b858e4010bffd0accc42e4a9e5b56b490e1c6a2
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-05-31 21:38:41 -0400

    Update Estimating _the_value_of_pi_using_Monte_Carlo_approach.py

--- Estimating _the_value_of_pi_using_Monte_Carlo_approach.py
+++ Estimating _the_value_of_pi_using_Monte_Carlo_approach.py
@@ -7,22 +7,29 @@ the no. of randomly generated points inside the
 square region. Dividing the pts inside by total
 will give a value equal to pi/4. 
 '''
-
 import numpy as np
 
-pt_in = 0.0
-pt_out = 0.0
+def pi_est(num_iter):
+    pt_in = 0.0
+    pt_out = 0.0
 
-# Generating 70,000 pts uniformly
-for i in range(70000):
+    # Generating num_iter uniformly
+    for i in range(num_iter):
 
-    x = np.random.uniform(0,1)
-    y = np.random.uniform(0,1)
+        x = np.random.uniform(0,1)
+        y = np.random.uniform(0,1)
 
-    if pow(x,2) + pow(y,2) <= 1:
-        pt_in += 1
+        if pow(x,2) + pow(y,2) <= 1:
+            pt_in += 1
 
-    else:
-        pt_out += 1
+        else:
+            pt_out += 1
 
-print(round(4*(pt_in/(pt_out + pt_in)),3))
+    pi = 4*(pt_in/(pt_in + pt_out))
+    pi = round(pi,3)
+    
+    return pi
+
+# Taking the iteration value from the user
+num_iter = int(input("Enter the no. of iterations: "))
+print(pi_est(num_iter))
