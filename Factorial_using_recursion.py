7c7058dacddf220859ff94a6f30dd60f45f29d35
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-06-05 00:44:28 -0400

    Update Factorial_using_recursion.py

--- Factorial_using_recursion.py
+++ Factorial_using_recursion.py
@@ -9,3 +9,21 @@ def fact(n):
 # Taking the input from the user
 n = int(input("Enter the no: "))
 print(fact(n))
+
+
+'''
+# Calculating factorial using for loop
+def fact(n):
+
+      fac = 1
+
+      for i in range(n-1):
+            fac = n*fac
+            n-= 1
+
+      return fac
+
+# Taking the input from the user
+n = int(input("Enter the no: "))
+print(fact(n))
+'''
