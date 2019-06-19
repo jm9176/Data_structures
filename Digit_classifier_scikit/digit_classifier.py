346ab0c6b3e002a04e6352861be193e2d2ac8dfd
Shubhdildeep Singh Sohal <50490953+jm9176@users.noreply.github.com>
2019-06-19 14:37:21 -0400

    Create digit_classifier.py

---
+++ Digit_classifier_scikit/digit_classifier.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from time import time
+from sklearn.tree import DecisionTreeClassifier
+
+# Importing the digit dataset
+data = pd.read_csv('train.csv').as_matrix()
+clf = DecisionTreeClassifier()
+
+# Total no. of samples in the inputs = 42000
+print("Total no. of samples: ", len(data))
+
+# training dataset = 35000 samples
+featureTrain= data[0:35000,1:]
+labelTrain = data[0:35000,0]
+
+# Measuring the training time for the input training samples
+# and fitting the model according to the input data
+t_train = time()
+clf.fit(featureTrain, labelTrain)
+print("Training time: ", round(time()-t_train,3),"secs")
+
+# testing dataset = 7000
+featureTest = data[35000:,1:]
+labelTest = data[35000:,0]
+
+# Measuring the training time for the test samples
+# and calculating the classifier score value
+t_test = time()
+score = clf.score(featureTest, labelTest)
+print("Test time: ", round(time()-t_test,3),"secs")
+print("Classifier accuracy: ", score)
+
+# Viewing a sample
+plt.figure()
+test_sample = np.reshape(featureTest[10],(28,28))
+print("Actual sample label: ", labelTest[10],"\nPredicted label: ", clf.predict([featureTest[10]]))
+plt.imshow(255 - test_sample, cmap = 'gray')
+plt.show()
